6467d9375ae2918610b09ba0d4861f4e24140e81
MaximeTR <maxime.tremblay-rheault@bdc.ca>
2023-05-19 09:24:53 -0400

    Arrays

---
+++ Arrays/MultiDimensional/Program.cs
@@ -0,0 +1,24 @@
+﻿int[,] Array2d = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+System.Console.WriteLine(Array2d[1, 2]);
+
+
+int[,,] Array3d = new int[2, 2, 3] {
+    {
+        {0, 1, 2}, {3, 4, 5}
+    },
+    {
+        {6, 7, 8}, {9, 10, 11}
+    }
+};
+
+System.Console.WriteLine(Array3d[1, 1, 2]);
+
+int[][] jaggedArray = new int[3][];
+jaggedArray[0] = new int[2] { 1, 2 };
+jaggedArray[1] = new int[3] { 3, 4, 5 };
+jaggedArray[2] = new int[4] { 6, 7, 8, 9 };
+
+System.Console.WriteLine(jaggedArray[0][0]);
+System.Console.WriteLine(jaggedArray[1][2]);
+System.Console.WriteLine(jaggedArray[2][3]);
---
+++ Arrays/SingleDimensional/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] values = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var items = new Item[2];
+items[0] = new Item("Guitar", 1);
+
+Console.WriteLine(values[0]);
+Console.WriteLine(items[0]);
+
+public record Item(string Name, int Price); // Needs to be the put at the end, because we're inside main method
+
