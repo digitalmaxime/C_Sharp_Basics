bd81d1edb690600d7d3c18b016ffa7b80094d967
MaximeTR <maxime.tremblay-rheault@bdc.ca>
2023-09-13 12:15:12 -0400

    dict from file

---
+++ Dictionary/DicFromFile/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] config = File.ReadAllLines(@"./config.txt");
+var dic = config
+  .Where(x => x != "")
+  .Select(x => x.Split('='))
+  .ToDictionary(x => x[0], x => x[1]);
+
+Console.WriteLine(dic);
